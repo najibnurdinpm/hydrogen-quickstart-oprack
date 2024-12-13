aba4dffc228c6ac9c8ee55cf885b37fd13ce1d7f
nurdin najib <najibnurdin999@gmail.com>
2024-12-13 18:14:43 +0700

    remove loader

--- app/root.tsx
+++ app/root.tsx
@@ -10,31 +10,7 @@ import type {LoaderArgs} from '@remix-run/cloudflare';
 import {defer} from '@remix-run/cloudflare';
 import type {Shop} from '@shopify/hydrogen/storefront-api-types';
 
-const LAYOUT_QUERY = `#graphql
-query layout {
-  shop {
-    name
-    description
-  }
-}
-`;
-
-export async function loader({context}: LoaderArgs) {
-
-  const {storefront} = await context.cloudflare;
-
-  if (!storefront) return null
-
-  const layout = await storefront.query(LAYOUT_QUERY)
-
-  return layout.shop;
-}
-
 export default function App() {
-
-  const data = useLoaderData<typeof loader>();
-
-  console.log('data', data)
   
 
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,34 +4,11 @@ import {
   useLoaderData,
 } from '@remix-run/react';
 
-const LAYOUT_QUERY = `#graphql
-query layout {
-  shop {
-    name
-    description
-  }
-}
-`;
-
-export async function loader({context}: LoaderArgs) {
-
-  const {storefront} = await context.cloudflare;
-
-  if (!storefront) return null
-
-  const layout = await storefront.query(LAYOUT_QUERY)
-
-  return layout.shop;
-}
-
 export default function Homepage() {
-
-  const data = useLoaderData<typeof loader>();
 
   return (
     <div className="home">
-      {!data ? ('loader sedang proses') : (data.name)}
-      
+      testing
     </div>
   );
 }
